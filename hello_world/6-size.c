3a03ada011c8a4398b51ed1f84d718168eacef4a
Kananmrzzd <146704930+Kananmrzzd@users.noreply.github.com>
2023-10-15 03:55:49 -0800

    Update 6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,10 +8,8 @@
  * Prints the size of various types based on
  * the computer it is compiled and run on..
  */
-
 int main(void)
 {
-  
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
