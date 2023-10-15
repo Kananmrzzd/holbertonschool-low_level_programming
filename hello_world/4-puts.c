1cadcaf976c3ed2ce23cea828cbc1fbc4263d7ad
Kananmrzzd <146704930+Kananmrzzd@users.noreply.github.com>
2023-10-15 03:40:46 -0800

    Update 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,18 +1,19 @@
 /*
  * File: 4-puts.c
- * Auth: Kanan <7748@holbertonschool.com>
+ * Auth: Kanan Mirzazada <7748@holbertonschool.com>
  */
+
 #include <stdio.h>
 
- /**
-  *  * main - Prints "Programming is like building a multilingual
-  *   *                puzzle, followed by a new line.
-  *    *
-  *     * Return: Always 0.
-  *      */
- int main(void)
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+	return (0);
 }
