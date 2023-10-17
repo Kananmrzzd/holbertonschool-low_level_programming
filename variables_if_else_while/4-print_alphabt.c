cd0114b5a33cd6ffe20883c41ed02370f98ff8b2
root <kananmirzazada@gmail.com>
2023-10-17 07:05:53 -0700

    a

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - main
+ *
+ *Return: void
+ */
+int main(void)
+{
+
+	char let;
+
+	for (let = 'a'; let <= 'z'; let++)
+	{
+
+		if (let != 'q' && let != 'e')
+		{
+
+			putchar(let);
+	                putchar('\n');
+		}
+	}
+
+	return (0);
+}
