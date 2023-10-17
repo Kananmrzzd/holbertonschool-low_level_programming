869b017d3f8022338843623613d70136a448f503
root <kananmirzazada@gmail.com>
2023-10-17 07:44:15 -0700

    a

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - main
+ *
+ *Return: void
+ */
+int main(void)
+{
+
+	char let;
+
+	for (let = 'z'; let <= 'a'; let--)
+		putchar(let);
+
+	putchar('\n');
+	return (0);
+}
