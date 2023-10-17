3997e9ad2abdccf7121d88e7ef2038d82fb10dd0
root <kananmirzazada@gmail.com>
2023-10-17 08:21:24 -0700

    a

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - main
+ *
+ *Return: void
+ */
+int main(void)
+{
+
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+
+		putchar(num);
+		putchar(',');
+		putchar(' ');
+	}
+
+	return (0);
+}
