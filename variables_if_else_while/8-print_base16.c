41ad5cc365fdb1b2a8dee08e84bf7d3814a4a4d3
Kananmrzzd <146704930+Kananmrzzd@users.noreply.github.com>
2023-10-17 07:40:36 -0800

    Update 8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -8,16 +8,21 @@
 int main(void)
 {
 	int num;
-
 	for (num = 0; num <= 16; num++)
 	{
 
-		if(num >= 10)
+		if(num < 10)
 		{
-			putchar((num/10) + '0');
+			
+			putchar(num + '0');
 		}
 
-		 putchar((num % 10) + '0');
+		else
+		{
+
+			putchar((num-10) + 'a');	
+		}
+
 	}
 
 	putchar('\n');
