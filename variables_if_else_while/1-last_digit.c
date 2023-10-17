33d0d4f6ebd9beb7a82d7cbb5b87f661e9a7bb7c
root <kananmirzazada@gmail.com>
2023-10-17 05:11:16 -0700

    a

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
+ * main - Entry point
  *
- *return: Always 0 (Succes)
+ * return: Always 0 (Succes)
  */
 int main(void)
 {
@@ -19,7 +19,7 @@ int main(void)
 	else if (n % 10 == 0)
 		printf("0\n");
 	else
-		printf("less than 6 and not 0");
+		printf("less than 6 and not 0\n");
 
 	return (0);
 }
