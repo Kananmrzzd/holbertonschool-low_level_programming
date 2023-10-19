e6de0bd961e7917220df0edee713a90e35ca1152
root <kananmirzazada@gmail.com>
2023-10-19 16:14:11 -0700

    a

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print to 98
+ * @n: integer
+ *
+ */
+void print_to_98(int n);
+{
+
+	while (n < 98 || n > 98)
+	{
+
+		n > 98 ? n-- : n++;
+		printf("%d, ", n);
+	}
+
+	printf("%d\n", n);
+}
