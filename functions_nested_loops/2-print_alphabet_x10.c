3c69c0ceba1773426c558b14b16214175b1dd152
root <kananmirzazada@gmail.com>
2023-10-19 11:08:30 -0700

    a

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * 10 x alphabet
+ */
+void print_alphabet(void)
+{
+
+	char c;
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+
+		for (c = 'a'; c <= 'z'; c++)
+		{
+
+			_putchar(c);
+		}
+
+		_putchar('\n');
+	}
+}
