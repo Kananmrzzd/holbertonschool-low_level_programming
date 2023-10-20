d5fc78f5c78089190c26ef39f91ff75995d50d21
root <kananmirzazada@gmail.com>
2023-10-20 00:52:16 -0700

    a

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -11,13 +11,27 @@ void times_table(void)
 	int n;
 	int m;
 
-	for(n = 0; n <= 9; n++)
+	for (n = 0; n <= 9; n++)
 	{
 
-		for(m = 0; m <= 9; m++)
+		for (m = 0; m <= 9; m++)
 		{
 
-			_putchar(n * m + 48);
+			_putchar('0');
+			_putchar(',');
+			_putchar(' ');
+
+			if (n * m <= 9 && n * m)
+			{
+
+				_putchar(n * m + 48);
+			}
+			else
+			{
+
+				_putchar(n * m / 10 + 48 );
+				_putchar(n * m % 10 + 48 );
+			}
 		}
 
 		_putchar('\n');
