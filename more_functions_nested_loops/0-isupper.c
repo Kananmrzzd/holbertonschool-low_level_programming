19788f1626b2df79586a44d13610179ce325f331
root <kananmirzazada@gmail.com>
2023-10-20 13:18:51 -0700

    a

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase
+ *
+ * @c - integer
+ *
+ * Return: int
+ */
+int _issupper(int c)
+{
+
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
+
