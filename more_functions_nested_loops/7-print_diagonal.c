86ade2d4caddfaab8cea88311e274a44f4d4fd09
root <kananmirzazada@gmail.com>
2023-10-21 03:47:57 -0700

    a

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a stright line
+ *
+ * @n: int
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+
+			_putchar('\\');
+			if (i != n-1)
+				_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
