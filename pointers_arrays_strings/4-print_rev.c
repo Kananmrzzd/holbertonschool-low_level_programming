15577e0124ee5442b04c802dfd4d05ebb8df3e56
root <kananmirzazada@gmail.com>
2023-10-26 00:44:32 -0700

    a

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,11 +6,19 @@
  */
 void print_rev(char *s)
 {
-	int len = _strlen(s);
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
 
 	while (len >= 0)
 	{
 		_putchar(s[len - 1]);
 		s--;
 	}
+
+	_putchar('\n');
 }
