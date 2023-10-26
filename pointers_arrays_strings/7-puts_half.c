3616dcc441524c596cf0173faee5d4f62110ca7a
root <kananmirzazada@gmail.com>
2023-10-26 06:39:39 -0700

    a

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - half of a string
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+
+	while (*(size + len) != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 != 0)
+		len+=1;
+
+	int i;
+
+	for ( i = 0; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n')
+}
