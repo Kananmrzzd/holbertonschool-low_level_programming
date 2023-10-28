f5c6c38e9845b08509a54f5bd2b745241df55539
root <kananmirzazada@gmail.com>
2023-10-28 11:53:44 -0700

    a

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -4,8 +4,15 @@
  * _strcmp - compares two string
  * @s1: char
  * @s2: char
+ * Return: int
  */
 int _strcmp(char *s1, char *s2)
 {
+	while(*s1 && *s2)
+	{
+		s1++;
+		s2++;
+	}
+
 	return (*s1 - *s2);
 }
