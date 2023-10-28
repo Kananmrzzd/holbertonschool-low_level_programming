7a06af9028f7ef3f7f2735e05459f73615ee3608
root <kananmirzazada@gmail.com>
2023-10-28 12:08:09 -0700

    a

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse array
+ * @a: ptr to an integer
+ * @n: int
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		int *ptr = *a[i];
+
+		while (*ptr != '\0')
+		{
+			_putchar(*ptr);
+			ptr++;
+		}
+	}
+}
