4fe125565ce2ce91bf67d67ea4624e5fe6130104
Kananmrzzd <146704930+Kananmrzzd@users.noreply.github.com>
2023-10-29 11:07:19 -0800

    Update 5-strstr.c

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strstr - Locates a substring.
+ * @haystack: ptr
+ * @needle: ptr
+ *
+ * Return: char
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		index = 0;
+
+		if (haystack[i] == needle[i])
+		{
+			while (haystack[i] == needle[i])
+      {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+
+				index++;
+
+			}
+		}
+
+		haystack++;
+	}
+
+	return ('\0');
+}
