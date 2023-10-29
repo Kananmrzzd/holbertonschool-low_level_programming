9296e6c5c1c885dee6cdb8fa75a5c9c5279bfd7d
root <kananmirzazada@gmail.com>
2023-10-29 12:28:52 -0700

    a

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 
 /**
@@ -34,43 +33,3 @@ char *_strstr(char *haystack, char *needle)
 
 	return ('\0');
 }
-=======
-#include "main.h"
-
-/**
- * _strstr - Locates a substring.
- * @haystack: ptr
- * @needle: ptr
- *
- * Return: char
- */
-
-char *_strstr(char *haystack, char *needle)
-{
-	int i;
-
-	if (*needle == 0)
-		return (haystack);
-
-	while (*haystack)
-	{
-		i = 0;
-
-		if (haystack[i] == needle[i])
-		{
-			while (haystack[i] == needle[i])
-      {
-				if (needle[i + 1] == '\0')
-					return (haystack);
-
-				i++;
-
-			}
-		}
-
-		haystack++;
-	}
-
-	return ('\0');
-}
->>>>>>> dd810883e372da563571e5dbcb99bd72c5a4fd6c
