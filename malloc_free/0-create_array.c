d5f67fb8ceafb80e4539622b9564b8deddf3c3af
Kananmrzzd <kananmirzazada@gmail.com>
2023-11-15 21:16:28 -0800

    a

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -8,13 +8,13 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	if (size == 0)
-		return NULL;
-
 	char *array;
 	unsigned int i;
 
-	array = (char *)malloc(sizeof(int) * size);
+	if (size == 0)
+		return NULL;
+
+	array = malloc(sizeof(int) * size);
 
 	for (i = 0; i < size; i++)
 		array[i] = c;
