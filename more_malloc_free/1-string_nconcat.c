ead584f6232817b68d8310295b7e0882a3bae50e
Kananmrzzd <kananmirzazada@gmail.com>
2023-11-19 09:59:04 -0800

    a

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - Concatenates two strings 
+ * string_nconcat - Concatenates two strings
  * @s1: The first string.
  * @s2: The second string.
  * @n: The maximum number of bytes of s2 to concatenate to s1.
