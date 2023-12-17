1c6b7126411d314d96fa40a79c2df44bf4885f9e
Kananmrzzd <kananmirzazada@gmail.com>
2023-12-17 10:46:21 -0800

    a

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,12 +2,12 @@
 #include "main.h"
 
 /**
- * read_textfile -  reads a text file and prints it 
+ * read_textfile -  reads a text file and prints it
  *                   to the POSIX standard output
- * @ filename: name of file
- * @ @letters: number of letters
+ * @filename: name of file
+ * @letters: number of letters
  *
- * Return: the actual number of 
+ * Return: the actual number of
  *          letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
