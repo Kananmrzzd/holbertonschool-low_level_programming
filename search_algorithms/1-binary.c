a6606a61a695d5dd9bcf3ae5c5c2aa868ffbe157
Kananmrzzd <kananmirzazada@gmail.com>
2024-01-21 23:29:06 +0400

    a

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,37 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in a sorted array of
+ *                 integers using the Binary search algorithm
+ * @array: integers
+ * @size: size of array
+ * @value: value 
+ * Return: int
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int i = 1;
+	int l = 0;
+	int r = size - 1;
+	int j;
+
+	if (!array)
+		return (-1);
+
+	for (i = 1; i < size; i*=2)
+	{
+		printf("Searching in array: ");
+		for (j = l; j < r; j++)
+			printf("%d, ", array[j]);
+		printf("%d\n", array[j]);
+
+		if (value > array[l + size / i])
+			l =l + size / i;
+		else if (value < array[l + size / i])
+			r = r - size / i;
+		else
+			return (array[l + size / i]);
+	}
+
+	return (-1);
+}
